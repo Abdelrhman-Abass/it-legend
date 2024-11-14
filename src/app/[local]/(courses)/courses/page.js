fa023ef261751dcc006c433eb619a64b14849db0
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-11-14 23:38:02 +0200

    edit for filter bug

--- src/app/[local]/(courses)/courses/page.js
+++ src/app/[local]/(courses)/courses/page.js
@@ -2,9 +2,13 @@
 import CourseTwoArea from "@/components/courses/CourseTwoArea";
 import { Wrapper } from "@/layout";
 import React, { useState } from "react";
+import { course_data } from "@/data";
+import SortingArea from "@/components/course-filter/sorting-area";
 
 const Page = () => {
   const [search, setSearch] = useState("");
+  const [courses, setCourses] = useState(course_data);
+
   const [category, setCategory] = useState("");
   const [maxPrice, setMaxPrice] = useState("");
 
@@ -12,10 +16,15 @@ const Page = () => {
     <Wrapper >
         
       <div className="container">
-
+      <SortingArea
+          course_items={course_data}
+          num={courses?.length}
+          setCourses={setCourses}
+          courses={courses}
+        />
         <CourseTwoArea />
-        <CourseTwoArea  />
-        <CourseTwoArea />
+        {/* <CourseTwoArea  />
+        <CourseTwoArea /> */}
 
       </div>
     </Wrapper>
