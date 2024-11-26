03563ab570bb8d94a6645ea839d63a6eaff718e4
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-11-26 16:16:57 +0200

    fix spacing

--- src/components/my-path/HeroArea.jsx
+++ src/components/my-path/HeroArea.jsx
@@ -49,7 +49,7 @@ const HeroArea = () => {
               <div className="edu-team-grid team-style-3 ">
                 <div className="inner ">
                   <div className="thumbnail-wrap ">
-                    <div className="thumbnail px-[50px]">
+                    <div className="thumbnail lg:px-[50px]">
                       <Link
                         href={`/course-player/c84e7902-1205-426f-a857-922bedd84bdf"`}
                         className="flex justify-center items-center"
