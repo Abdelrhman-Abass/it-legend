d2939ed22afc69cc384e76754b633ac41dc31db7
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-12-02 12:16:57 +0200

    fix not found bug

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,6 +1,6 @@
 // This will be specific to your locale routes
 import Image from 'next/image'
-import { Link } from "@/navigation";
+import Link from 'next/link'
 
 export default function LocaleNotFound() {
   const direction = "ltr"
@@ -9,7 +9,9 @@ export default function LocaleNotFound() {
       <Image src={"/assets/images/others/404.png"} width={350} height={350} />
       <h2 className="title text-6xl pt-[30px] pb-[10px] font-bold mb-4 text-center ">404 - Page Not Found</h2>
       <p className="mb-4 text-center">The page you are looking for doesn't exist</p>
+      
       <Link className="m-0 edu-btn m-0" href="/" style={{ cursor: "pointer" }}>
+        
           <i className="icon-4 mr-2"></i>
         <span>Return Home</span>
       </Link>
