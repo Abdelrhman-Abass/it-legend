1ae410733de410801ec9664df2259df384498c17
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-12-08 13:32:16 +0200

    test

--- src/middleware.js
+++ src/middleware.js
@@ -33,8 +33,9 @@ export async function middleware(req) {
   // }
 
   // If user is not logged in and trying to access private paths, redirect to login
-  if (!isUserLoggedIn && privatePaths.some((path) => pathname.includes(path)))
-     {
+  if (!isUserLoggedIn && privatePaths.some((path) => pathname.includes(path)) ||
+    pathname == "/ar" ||
+    pathname == "/en") {
     return NextResponse.redirect(new URL("/auth", req.url));
   }
 
