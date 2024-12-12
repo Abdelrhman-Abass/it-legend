65aa598541a36197c3fa846c8c7360bb13afd34d
Abdelruhman Abass <92057185+Abdelrhman-Abass@users.noreply.github.com>
2024-12-12 15:53:12 +0200

    add nodes in course details

--- src/app/[local]/(courses)/course-details/[id]/page.js
+++ src/app/[local]/(courses)/course-details/[id]/page.js
@@ -2,9 +2,13 @@ import CourseBreadcrumb from "@/components/breadcrumb/breadcrumb-5";
 import CourseDetailsArea from "@/components/course-details/course-details-area";
 import Testimonial from "@/components/diploma/Testimonial";
 import { course_data } from "@/data";
+import { CoursePlayerNode } from "@/hooks/PlayerHandler";
+
 import React from "react";
 const course = course_data[0];
-const page = () => {
+const page = async ({params}) => {
+  const courseNode = await CoursePlayerNode(params.id)
+console.log(courseNode)
   return (
     <>
       <CourseBreadcrumb
